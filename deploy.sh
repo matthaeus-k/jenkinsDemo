7d3553985feb5fb045c2d64dede87d8719d58581
matthaeus <sunghwan.kwon@emotion.co.kr>
2022-02-13 21:08:10 +0900

    deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Hello World"
-sudo mkdir -p /usr/local/tomcat
+mkdir -p /usr/local/tomcat
+mkdir -p /opt/node
+wget -O node-v14.19.0-linux-arm64.tar.gz https://nodejs.org/download/release/latest-v14.x/node-v14.19.0-linux-arm64.tar.gz
+unzip -d /opt/node node-v14.19.0-linux-arm64.tar.gz
