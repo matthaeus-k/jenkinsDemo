4180ab55a133ceaf8502f28119b024399da10edb
matthaeus <sunghwan.kwon@emotion.co.kr>
2022-02-14 12:36:36 +0900

    20220214-1.2

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 echo "Hello World war 파일 톰캣 경로"
 sudo mkdir -p /var/webapps/
-sudo copy /var/lib/jenkins/workspace/Demo1/build/libs/*.war /var/webapps/ROOT.war
+sudo rm -rf /var/webapps/*
+sudo cp -p /var/lib/jenkins/workspace/Demo1/build/libs/Demo.war /var/webapps/ROOT.war
